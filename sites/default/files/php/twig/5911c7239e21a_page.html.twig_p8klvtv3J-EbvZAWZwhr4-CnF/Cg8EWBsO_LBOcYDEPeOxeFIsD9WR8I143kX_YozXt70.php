47c98c69b382855e0587fca15c8a6372611c7ba2
RJ Toney <rjtoney@RJs-MacBook-Pro.local>
2017-05-11 15:10:43 -0400

    Another push

---
+++ sites/default/files/php/twig/5911c7239e21a_page.html.twig_p8klvtv3J-EbvZAWZwhr4-CnF/Cg8EWBsO_LBOcYDEPeOxeFIsD9WR8I143kX_YozXt70.php
@@ -0,0 +1,294 @@
+<?php
+
+/* themes/kaytee_dog/templates/system/page.html.twig */
+class __TwigTemplate_95788c89f8b105a43b9bfa63d6128af3ad2b0cebacde5d5bac3c45f3e177550e extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = array(
+            'main' => array($this, 'block_main'),
+            'content' => array($this, 'block_content'),
+            'sidebar_second' => array($this, 'block_sidebar_second'),
+            'footer' => array($this, 'block_footer'),
+        );
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        $tags = array("set" => 59, "block" => 72, "if" => 106);
+        $filters = array();
+        $functions = array();
+
+        try {
+            $this->env->getExtension('sandbox')->checkSecurity(
+                array('set', 'block', 'if'),
+                array(),
+                array()
+            );
+        } catch (Twig_Sandbox_SecurityError $e) {
+            $e->setTemplateFile($this->getTemplateName());
+
+            if ($e instanceof Twig_Sandbox_SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof Twig_Sandbox_SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof Twig_Sandbox_SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+        // line 59
+        $context["container"] = (($this->getAttribute($this->getAttribute((isset($context["theme"]) ? $context["theme"] : null), "settings", array()), "fluid_container", array())) ? ("container-fluid") : ("container"));
+        // line 60
+        echo "
+<div class=\"container\">
+  <div class=\"row\">
+    <div id=\"logo\" class=\"col-sm-1 col-md-4 col-lg-2\"><img src=\"/e3_challenge-d8/themes/kaytee_dog/logo.png\"></a></div> <!-- logo -->
+    <div id=\"bar\" class=\"col-sm-3 col-md-8 col-lg-10\">       </div>
+
+    <div class=\"col-md-8 col-lg-8 pull-right\">";
+        // line 66
+        echo $this->env->getExtension('sandbox')->ensureToStringAllowed($this->env->getExtension('drupal_core')->escapeFilter($this->env, $this->getAttribute((isset($context["page"]) ? $context["page"] : null), "navigation", array()), "html", null, true));
+        echo "</div> <!-- col md 5 --></div> <!-- nav -->
+  </div> <!-- row -->
+</div> <!-- container -->
+
+</div>
+";
+        // line 72
+        $this->displayBlock('main', $context, $blocks);
+        // line 105
+        echo "
+";
+        // line 106
+        if ($this->getAttribute((isset($context["page"]) ? $context["page"] : null), "footer", array())) {
+            // line 107
+            echo "  ";
+            $this->displayBlock('footer', $context, $blocks);
+        }
+    }
+
+    // line 72
+    public function block_main($context, array $blocks = array())
+    {
+        // line 73
+        echo "  <div role=\"main\" class=\"main-container ";
+        echo $this->env->getExtension('sandbox')->ensureToStringAllowed($this->env->getExtension('drupal_core')->escapeFilter($this->env, (isset($context["container"]) ? $context["container"] : null), "html", null, true));
+        echo " js-quickedit-main-content\">
+    <div class=\"row\">
+
+      ";
+        // line 77
+        echo "      ";
+        // line 78
+        $context["content_classes"] = array(0 => ((($this->getAttribute(        // line 79
+(isset($context["page"]) ? $context["page"] : null), "sidebar_first", array()) && $this->getAttribute((isset($context["page"]) ? $context["page"] : null), "sidebar_second", array()))) ? ("col-sm-6") : ("")), 1 => ((($this->getAttribute(        // line 80
+(isset($context["page"]) ? $context["page"] : null), "sidebar_first", array()) && twig_test_empty($this->getAttribute((isset($context["page"]) ? $context["page"] : null), "sidebar_second", array())))) ? ("col-sm-9") : ("")), 2 => ((($this->getAttribute(        // line 81
+(isset($context["page"]) ? $context["page"] : null), "sidebar_second", array()) && twig_test_empty($this->getAttribute((isset($context["page"]) ? $context["page"] : null), "sidebar_first", array())))) ? ("col-sm-9") : ("")), 3 => (((twig_test_empty($this->getAttribute(        // line 82
+(isset($context["page"]) ? $context["page"] : null), "sidebar_first", array())) && twig_test_empty($this->getAttribute((isset($context["page"]) ? $context["page"] : null), "sidebar_second", array())))) ? ("col-sm-12") : ("")));
+        // line 85
+        echo "      <section";
+        echo $this->env->getExtension('sandbox')->ensureToStringAllowed($this->env->getExtension('drupal_core')->escapeFilter($this->env, $this->getAttribute((isset($context["content_attributes"]) ? $context["content_attributes"] : null), "addClass", array(0 => (isset($context["content_classes"]) ? $context["content_classes"] : null)), "method"), "html", null, true));
+        echo ">
+
+        ";
+        // line 88
+        echo "        ";
+        $this->displayBlock('content', $context, $blocks);
+        // line 92
+        echo "      </section>
+
+      ";
+        // line 95
+        echo "      ";
+        if ($this->getAttribute((isset($context["page"]) ? $context["page"] : null), "sidebar_second", array())) {
+            // line 96
+            echo "        ";
+            $this->displayBlock('sidebar_second', $context, $blocks);
+            // line 101
+            echo "      ";
+        }
+        // line 102
+        echo "    </div>
+  </div>
+";
+    }
+
+    // line 88
+    public function block_content($context, array $blocks = array())
+    {
+        // line 89
+        echo "          <a id=\"main-content\"></a>
+          ";
+        // line 90
+        echo $this->env->getExtension('sandbox')->ensureToStringAllowed($this->env->getExtension('drupal_core')->escapeFilter($this->env, $this->getAttribute((isset($context["page"]) ? $context["page"] : null), "content", array()), "html", null, true));
+        echo "
+        ";
+    }
+
+    // line 96
+    public function block_sidebar_second($context, array $blocks = array())
+    {
+        // line 97
+        echo "          <aside class=\"col-sm-3\" role=\"complementary\">
+            ";
+        // line 98
+        echo $this->env->getExtension('sandbox')->ensureToStringAllowed($this->env->getExtension('drupal_core')->escapeFilter($this->env, $this->getAttribute((isset($context["page"]) ? $context["page"] : null), "sidebar_second", array()), "html", null, true));
+        echo "
+          </aside>
+        ";
+    }
+
+    // line 107
+    public function block_footer($context, array $blocks = array())
+    {
+        // line 108
+        echo "    <footer class=\"footer ";
+        echo $this->env->getExtension('sandbox')->ensureToStringAllowed($this->env->getExtension('drupal_core')->escapeFilter($this->env, (isset($context["container"]) ? $context["container"] : null), "html", null, true));
+        echo "\" role=\"contentinfo\">
+      ";
+        // line 109
+        echo $this->env->getExtension('sandbox')->ensureToStringAllowed($this->env->getExtension('drupal_core')->escapeFilter($this->env, $this->getAttribute((isset($context["page"]) ? $context["page"] : null), "footer", array()), "html", null, true));
+        echo "
+    </footer>
+  ";
+    }
+
+    public function getTemplateName()
+    {
+        return "themes/kaytee_dog/templates/system/page.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  157 => 109,  152 => 108,  149 => 107,  142 => 98,  139 => 97,  136 => 96,  130 => 90,  127 => 89,  124 => 88,  118 => 102,  115 => 101,  112 => 96,  109 => 95,  105 => 92,  102 => 88,  96 => 85,  94 => 82,  93 => 81,  92 => 80,  91 => 79,  90 => 78,  88 => 77,  81 => 73,  78 => 72,  72 => 107,  70 => 106,  67 => 105,  65 => 72,  57 => 66,  49 => 60,  47 => 59,);
+    }
+
+    public function getSource()
+    {
+        return "{#
+/**
+ * @file
+ * Default theme implementation to display a single page.
+ *
+ * The doctype, html, head and body tags are not in this template. Instead they
+ * can be found in the html.html.twig template in this directory.
+ *
+ * Available variables:
+ *
+ * General utility variables:
+ * - base_path: The base URL path of the Drupal installation. Will usually be
+ *   \"/\" unless you have installed Drupal in a sub-directory.
+ * - is_front: A flag indicating if the current page is the front page.
+ * - logged_in: A flag indicating if the user is registered and signed in.
+ * - is_admin: A flag indicating if the user has permission to access
+ *   administration pages.
+ *
+ * Site identity:
+ * - front_page: The URL of the front page. Use this instead of base_path when
+ *   linking to the front page. This includes the language domain or prefix.
+ *
+ * Navigation:
+ * - breadcrumb: The breadcrumb trail for the current page.
+ *
+ * Page content (in order of occurrence in the default page.html.twig):
+ * - title_prefix: Additional output populated by modules, intended to be
+ *   displayed in front of the main title tag that appears in the template.
+ * - title: The page title, for use in the actual content.
+ * - title_suffix: Additional output populated by modules, intended to be
+ *   displayed after the main title tag that appears in the template.
+ * - messages: Status and error messages. Should be displayed prominently.
+ * - tabs: Tabs linking to any sub-pages beneath the current page (e.g., the
+ *   view and edit tabs when displaying a node).
+ * - action_links: Actions local to the page, such as \"Add menu\" on the menu
+ *   administration interface.
+ * - node: Fully loaded node, if there is an automatically-loaded node
+ *   associated with the page and the node ID is the second argument in the
+ *   page's path (e.g. node/12345 and node/12345/revisions, but not
+ *   comment/reply/12345).
+ *
+ * Regions:
+ * - page.header: Items for the header region.
+ * - page.navigation: Items for the navigation region.
+ * - page.navigation_collapsible: Items for the navigation (collapsible) region.
+ * - page.highlighted: Items for the highlighted content region.
+ * - page.help: Dynamic help text, mostly for admin pages.
+ * - page.content: The main content of the current page.
+ * - page.sidebar_first: Items for the first sidebar.
+ * - page.sidebar_second: Items for the second sidebar.
+ * - page.footer: Items for the footer region.
+ *
+ * @ingroup templates
+ *
+ * @see template_preprocess_page()
+ * @see html.html.twig
+ */
+#}
+{% set container = theme.settings.fluid_container ? 'container-fluid' : 'container' %}
+
+<div class=\"container\">
+  <div class=\"row\">
+    <div id=\"logo\" class=\"col-sm-1 col-md-4 col-lg-2\"><img src=\"/e3_challenge-d8/themes/kaytee_dog/logo.png\"></a></div> <!-- logo -->
+    <div id=\"bar\" class=\"col-sm-3 col-md-8 col-lg-10\">       </div>
+
+    <div class=\"col-md-8 col-lg-8 pull-right\">{{ page.navigation }}</div> <!-- col md 5 --></div> <!-- nav -->
+  </div> <!-- row -->
+</div> <!-- container -->
+
+</div>
+{# Main #}
+{% block main %}
+  <div role=\"main\" class=\"main-container {{ container }} js-quickedit-main-content\">
+    <div class=\"row\">
+
+      {# Content #}
+      {%
+        set content_classes = [
+          page.sidebar_first and page.sidebar_second ? 'col-sm-6',
+          page.sidebar_first and page.sidebar_second is empty ? 'col-sm-9',
+          page.sidebar_second and page.sidebar_first is empty ? 'col-sm-9',
+          page.sidebar_first is empty and page.sidebar_second is empty ? 'col-sm-12'
+        ]
+      %}
+      <section{{ content_attributes.addClass(content_classes) }}>
+
+        {# Content #}
+        {% block content %}
+          <a id=\"main-content\"></a>
+          {{ page.content }}
+        {% endblock %}
+      </section>
+
+      {# Sidebar Second #}
+      {% if page.sidebar_second %}
+        {% block sidebar_second %}
+          <aside class=\"col-sm-3\" role=\"complementary\">
+            {{ page.sidebar_second }}
+          </aside>
+        {% endblock %}
+      {% endif %}
+    </div>
+  </div>
+{% endblock %}
+
+{% if page.footer %}
+  {% block footer %}
+    <footer class=\"footer {{ container }}\" role=\"contentinfo\">
+      {{ page.footer }}
+    </footer>
+  {% endblock %}
+{% endif %}
+";
+    }
+}
